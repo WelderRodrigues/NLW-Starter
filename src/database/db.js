43f02a7de45966c3df22baaa7942657a792fe609
Welder Rodrigues <welder_15@hotmail.com>
2020-06-14 22:49:22 -0300

    Enviando dados
    Enviando dados do front para o back end pelo formulario com o metodo post

--- src/database/db.js
+++ src/database/db.js
@@ -2,7 +2,9 @@ const sqlite3 = require('sqlite3').verbose()
 
 const db = new sqlite3.Database('./src/database/database.db')
 
+
 db.serialize(() => {
+    /*
     db.run(`
     CREATE TABLE IF NOT EXISTS places (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -45,6 +47,25 @@ db.serialize(() => {
         console.log(this)
     }
 
-    db.run(query, values, afterInsertData)
+    //db.run(query, values, afterInsertData)
+
+    db.all(`SELECT * FROM places`, function(error, rows) {
+        if(error){
+            return console.log(error)
+        }
+        console.log('Todos os Registros: ')
+        console.log(rows)
+    })
+    
+    db.run(`DELETE FROM places WHERE id = ?`, [1], function(error) {
+        if (error){
+            return console.log(error)
+        }
+
+        console.log('Registro deletado com sucesso!')
+    })
+    */
 })
+//*/
+module.exports = db
 
--- src/server.js
+++ src/server.js
@@ -1,26 +1,74 @@
-const express = require('express')
-const nunjucks = require('nunjucks')
+const express = require("express");
+const nunjucks = require("nunjucks");
+const database = require("./database/db.js");
 
+const server = express();
 
-const server = express()
+server.use(express.static("public"));
 
-server.use(express.static('public'))
+server.use(express.urlencoded({ extended: true }));
 
-nunjucks.configure('src/views', {
-    express: server,
-    noCache: true
-})
+nunjucks.configure("src/views", {
+  express: server,
+  noCache: true,
+});
 
-server.get('/', (request, response) => {
-    return response.render('index.html')
-})
+server.get("/", (request, response) => {
+  return response.render("index.html");
+});
 
-server.get('/create-point', (request, response) => {
-    return response.render('create-point.html')
-})
+server.get("/create-point", (request, response) => {
+  return response.render("create-point.html");
+});
 
-server.get('/search', (request, response) => {
-    return response.render('search-results.html')
-})
+server.post("/create-point", (request, response) => {
+  const query = `
+    INSERT INTO places (
+        image,
+        name,
+        address,
+        address2,
+        state,
+        city,
+        items
+    ) VALUES (?,?,?,?,?,?,? );
+`;
+  const values = [
+    request.body.image,
+    request.body.name,
+    request.body.address,
+    request.body.address2,
+    request.body.state,
+    request.body.city,
+    request.body.items
+  ];
 
-server.listen(3000)
+  function afterInsertData(error) {
+    if (error) {
+      return console.log(error);
+    }
+    console.log("Cadastrado com sucesso");
+    console.log(this);
+
+    return response.send("Foi");
+  }
+
+  database.run(query, values, afterInsertData);
+});
+
+server.get("/search", (request, response) => {
+  database.all(`SELECT * FROM places`, (error, rows) => {
+    if (error) {
+      return console.log(error);
+    }
+
+    const total = rows.length;
+
+    return response.render("search-results.html", {
+      places: rows,
+      total: total,
+    });
+  });
+});
+
+server.listen(3000);
